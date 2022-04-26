6052d3e5ed1dfb8d89c320cede43bf57ca62784b
Owais <kowais915@gmail.com>
2022-04-26 22:31:23 +0500

    added a form

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+//the form
+
+const searchForm = document.querySelector('form');
+
+searchForm.addEventListener('submit', (event=>{
+    event.preventDefault();
+
+    const dt = searchForm.search.value.trim();
+    console.log(dt);
+}))
+
+ 
+
 // we will be fetching data from the Mars Rover API
 const apK = "7k2p6Ys2NIb4N6A6n771LTgCOapH2YBh9JlAy1G9";
 const base = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=";
@@ -14,8 +27,15 @@ const goFitch = async (resource)=> {
 
 
 console.log("working");
+
+
+let roverName = 0;
 goFitch(comb).then(data=>{
-    console.log(data.photos[1].id);
+    roverName = data.photos[1].camera.name;
+    console.log(roverName);
 }).catch(err=>{
     console.log(err);
 })
+
+let ptag = document.querySelector("p");
+ptag.innerHTML += `<strong> Rover Name: ${roverName}</strong>`;
